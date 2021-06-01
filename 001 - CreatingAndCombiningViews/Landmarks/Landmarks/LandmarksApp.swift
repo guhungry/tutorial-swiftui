0ee2e1e34235b5f84867cc3a52b455f2f667b07b
Woraphot Chokratanasombat <guhungry@gmail.com>
2021-06-01 13:49:18 +0700

    [Landmarks] Change: Finished up first tutorial

---
+++ 001 - CreatingAndCombiningViews/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Woraphot Chokratansombat on 1/6/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
