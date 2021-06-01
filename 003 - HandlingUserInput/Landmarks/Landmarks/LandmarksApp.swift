767460477108fb95ec4cd83c03cec42fa585cdc0
Woraphot Chokratanasombat <guhungry@gmail.com>
2021-06-01 16:10:47 +0700

    [Handling User Input] Change: Finished up third tutorial

---
+++ 003 - HandlingUserInput/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Woraphot Chokratansombat on 1/6/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
