c6f718089781a4fc31c10b954711fc1705eeea57
Woraphot Chokratanasombat <guhungry@gmail.com>
2021-06-01 23:10:58 +0700

    [Working with UI Controls] Change: Finished up seventh tutorial

---
+++ 007 - WorkingWithUIControls/Landmarks/Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Woraphot Chokratansombat on 1/6/2564 BE.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
